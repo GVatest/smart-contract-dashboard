ba6708f64a727f95d1a463246eaada3d88f2bb6a
Gvatest <ganjatriller@gmail.com>
2023-03-30 14:14:06 +0300

    add body default background color

--- src/shared/config/theme.ts
+++ src/shared/config/theme.ts
@@ -1,20 +1,20 @@
-import { createTheme, Palette } from '@mui/material';
-import background from 'static/brand/background.svg';
+import { createTheme, Palette } from "@mui/material";
+import background from "static/brand/background.svg";
 
 const defaultTheme = createTheme({
   palette: {
     primary: {
-      main: '#20262D',
-      light: 'rgba(255, 255, 255, 0.07)',
-      dark: '#101418',
-      contrastText: '#efc947',
+      main: "#20262D",
+      light: "rgba(255, 255, 255, 0.07)",
+      dark: "#101418",
+      contrastText: "#efc947",
     },
     secondary: {
-      main: '#877e4b',
+      main: "#877e4b",
     },
     text: {
-      primary: '#fff',
-      secondary: 'rgba(255, 255, 255, 0.8)',
+      primary: "#fff",
+      secondary: "rgba(255, 255, 255, 0.8)",
     },
   },
   shape: {
@@ -29,11 +29,12 @@ const defaultTheme = createTheme({
     MuiCssBaseline: {
       styleOverrides: {
         body: {
-          minWidth: '100vw',
-          minHeight: '100vh',
+          minWidth: "100vw",
+          minHeight: "100vh",
+          backgroundColor: "#000",
           backgroundImage: `url("${background}")`,
-          backgroundRepeat: 'no-repeat, repeat',
-          backgroundSize: 'cover',
+          backgroundRepeat: "no-repeat, repeat",
+          backgroundSize: "cover",
         },
       },
     },
@@ -48,7 +49,7 @@ const {
 let theme = createTheme({
   ...defaultTheme,
   typography: (palette: Palette) => ({
-    fontFamily: 'Poppins, sans-serif',
+    fontFamily: "Poppins, sans-serif",
     h1: {
       fontWeight: 700,
       fontSize: pxToRem(22),
@@ -61,24 +62,24 @@ let theme = createTheme({
     },
     h3: {
       fontWeight: 600,
-      fontSize: '1rem',
-      [breakpoints.down('md')]: {
-        fontSize: '0.9rem',
+      fontSize: "1rem",
+      [breakpoints.down("md")]: {
+        fontSize: "0.9rem",
       },
       color: palette.text.primary,
     },
     h4: {
       fontWeight: 500,
-      fontSize: '1rem',
-      [breakpoints.down('md')]: {
-        fontSize: '0.9rem',
+      fontSize: "1rem",
+      [breakpoints.down("md")]: {
+        fontSize: "0.9rem",
       },
       color: palette.text.secondary,
     },
     button: {
       fontSize: pxToRem(18),
-      [breakpoints.down('md')]: {
-        fontSize: '1rem',
+      [breakpoints.down("md")]: {
+        fontSize: "1rem",
       },
     },
   }),
